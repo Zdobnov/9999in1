ec37d2e0810324ccac28be0cdca0978f5f5ceb40
Zdobnov <dr.ankh@gmail.com>
2019-04-07 22:51:08 +0200

    click event added, only video exists

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 window.onload = function() {
   var video = document.getElementById('video');
+  var play = document.getElementById('play');
   var tvScreen = document.getElementsByClassName('tv__screen')[0];
-
-  tvScreen.classList.add('glowing');
 
   function handleVideoEnd() {
     video.currentTime = 0;
 
-    video.removeEventListener('ended', handleVideoEnd);
-    video.remove();
+    video.style.display = 'none';
+    tvScreen.classList.remove('glowing');
   }
 
+  async function handleClickPlay() {
+    try {
+      await video.play();
+
+      video.style.display = 'block';
+      tvScreen.classList.add('glowing');
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  play.addEventListener('click', handleClickPlay);
   video.addEventListener('ended', handleVideoEnd);
 };
