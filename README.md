87c02c3cbf2cce3b63d40cf748dffcbc34a24288
Zdobnov <dr.ankh@gmail.com>
2019-04-07 21:36:06 +0200

    link fixed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://zdobnov.github.io/9999in1/
+
 # 9999in1
 8 bit
 
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ window.onload = function() {
   var video = document.getElementById('video');
 
   function handleVideoEnd() {
+    video.currentTime = 0;
+
     video.removeEventListener('ended', handleVideoEnd);
     video.remove();
   }
